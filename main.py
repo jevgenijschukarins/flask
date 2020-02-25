34e2c20314b09085d266a674e706bca144f50393
jevgenijschukarins <56860675+jevgenijschukarins@users.noreply.github.com>
2020-02-25 07:58:26 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sfrom flask import Flask, render_template
-
+from flask import Flask, render_template, request
+from file_proc import read_file, write_file
 
 app = Flask(__name__)
 
@@ -7,31 +7,49 @@ app = Flask(__name__)
 def index():
   return "Hi!"
 
-@app.route('/home', aktiva_lapa = 'home')
+@app.route('/home')
 def home():
-  return"<h1><a href='/about'>my home</a></h1>"
+  return render_template('home.html')
 
-@app.route('/about', aktiva_lapa = 'about')
-def about():
+@app.route('/about')
+def about_html():
   return render_template('about.html')
 
-@app.route('/contact', aktiva_lapa = 'contact')
-def contact():
-  return render_template('contact.html',Phone=29647470)
+@app.route('/contact')
+def contacts_html():
+  return render_template('contact.html', phone = "22140077")
 
 @app.route('/params')
 def params():
-  args= request.args
-  for key, value in args.items():
-    print(f"{key}:{value}")
-  return args
+  return request.args
 
-@app.route('/params_table')
-def params_table():
-  args= request.args
+@app.route('/post', methods = ['POST'])
+def post():
+  return request.get_json()
 
-  return render_template('params_table.html', args = args)
+@app.route('/read_file')
+def read_from_file():
+  content = read_file()
+  return content
 
-if __name__ =='__main__':
-  app.run(host="0.0.0.0",port=5232, threaded = True , debug = True) 
+@app.route('/write_file', methods = ['POST'])
+def write_to_file():
+  content_type = request.content_type
+  if content_type == 'application/json':
+    contentJSON = request.get_json()
+    write_file(contentJSON['data'])
+    return f"Add line {contentJSON['data']} to file."
+  else:
+    return f"Content type {content_type} is not supported!"
+  
+@app.route('/file', methods = ['GET', 'POST'])
+def workFile():
+  if request.method == 'GET':
+    return read_from_file()
+  elif request.method == 'POST':
+    return write_to_file()
+  else:
+    return f"Method {request.method} is not supported!"
 
+if __name__ == '__main__':
+  app.run(host="0.0.0.0", threaded=True, port=5020, debug=True)
